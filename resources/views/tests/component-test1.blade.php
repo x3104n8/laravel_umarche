2876a3b598f79fd9fa2389382235b1822c016952
Shinya Satoh <x3104n8@gmail.com>
2025-03-20 14:04:30 +0900

    sec02_27_componentAttributeBag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,7 @@
 <x-tests.app>
     <x-slot name="header1">名前付きヘッダ1</x-slot>
 コンポーネントテスト1
-    <x-tests.card :message="$message" title="タイトルだよ" content="本文だよ"/>
-    <x-tests.card                     title="タイトル2" />
+    <x-tests.card title="タイトルだよ" content="本文だよ" :message="$message" />
+    <x-tests.card title="タイトル2" />   <!-- :message,  contentは tests.card 側の初期値が採用される -->
+    <x-tests.card title="タイトル2" class="bg-red-300"/>
 </x-tests.app>
