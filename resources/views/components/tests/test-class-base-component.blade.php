a5d5362a6d1aab3900049f221841aba609ed1a85
Shinya Satoh <x3104n8@gmail.com>
2025-03-20 15:57:32 +0900

    sec02_28_componentClassBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div> {{ $classBaseMessage }} </div>
+    <div> {{ $defaultMessage }} </div>
+    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
     <x-slot name="header1">名前付きヘッダ2</x-slot>
 コンポーネントテスト2
+    <x-test-class-base classBaseMessage="メッセージ" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージ" defaultMessage="初期値から変更しています。" />
 </x-tests.app>
