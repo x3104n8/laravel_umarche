4bed0d9280e46006d7171b1d84150676a791d80e
Shinya Satoh <x3104n8@gmail.com>
2025-03-20 13:41:08 +0900

    sec02_26_componentProps

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    'title' => 'タイトルの初期値',
+    'message' => 'messageの初期値です',
+    'content' => '本文の初期値です',
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div> {{ $title }} </div>
     <div> 画像 </div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
     <x-slot name="header1">名前付きヘッダ1</x-slot>
 コンポーネントテスト1
     <x-tests.card :message="$message" title="タイトルだよ" content="本文だよ"/>
+    <x-tests.card                     title="タイトル2" />
 </x-tests.app>
