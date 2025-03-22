5203b6c8bee96988e39cf4bfce794709be4e0e28
Shinya Satoh <x3104n8@gmail.com>
2025-03-22 16:37:37 +0900

    sec03_36_serviceProviderTest

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -27,6 +27,14 @@ class LifeCycleTestController extends Controller
 
         dd($test, app());
     }
+    public function showServiceProviderTest(){
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample, $password, $encrypt->decrypt($password));
+    }
 }
 
 class Sample{
